6b2c7a4a79c89849aeb1676e81ce27b0a12e961e
Zain Ali <zainali20010579@gmail.com>
2023-05-08 16:19:04 +0200

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-};
+module.exports = nextConfig

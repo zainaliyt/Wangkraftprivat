1fc135b1d31d87d1cdc7f9c17b5f39e8dbfa181d
Zain Ali <zainali20010579@gmail.com>
2023-05-08 16:00:56 +0200

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  distDir: 'build',
+  images: {
+    loader: 'akamai',
+    path: '',
+  }
+  
 }
 
 module.exports = nextConfig
